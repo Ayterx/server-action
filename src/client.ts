634ec9ff426d3e80f92a3b82784b2ff6cb8615b9
Ayterx <79310652+Ayterx@users.noreply.github.com>
2024-03-27 21:13:00 +0300

    feat: add onSuccess and onError events

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,27 @@
 'use client'
 
+import { useEffect } from 'react'
 import { useFormState } from 'react-dom'
+import type { ActionReturnType } from './server/types'
 
 export const useAction = <ActionReturnGeneric>(
-  action: (currentState: unknown, from: FormData) => Promise<ActionReturnGeneric>
+  action: (currentState: unknown, from: FormData) => ActionReturnType<ActionReturnGeneric>,
+  events?: {
+    onSuccess?: (state: ActionReturnGeneric) => void
+    onError?: (error: { message: string }) => void
+  }
 ) => {
   const [state, internelAction] = useFormState(action, null)
+
+  useEffect(() => {
+    if (state && events) {
+      if (state.status === 'success' && events.onSuccess) {
+        events.onSuccess(state.data)
+      } else if (state.status === 'error' && events.onError) {
+        events.onError({ message: state.message })
+      }
+    }
+  }, [state])
 
   return {
     state,
