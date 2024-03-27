1d8c56bac210722920a81dd75826405cba88c39f
Ayterx <79310652+Ayterx@users.noreply.github.com>
2024-03-27 21:23:47 +0300

    chore: Add a description to the events for clarification

--- src/client.ts
+++ src/client.ts
@@ -7,7 +7,13 @@ import type { ActionReturnType } from './server/types'
 export const useAction = <ActionReturnGeneric>(
   action: (currentState: unknown, from: FormData) => ActionReturnType<ActionReturnGeneric>,
   events?: {
+    /**
+     * @description This event will be triggered in the client side when the action is successful
+     */
     onSuccess?: (state: ActionReturnGeneric) => void
+    /**
+     * @description This event will be triggered in the client side when the action is failed
+     */
     onError?: (error: { message: string }) => void
   }
 ) => {
