9dbe302f6fe8c2836bd947b13d3d9f6865b58c80
Ayterx <79310652+Ayterx@users.noreply.github.com>
2024-03-27 21:16:57 +0300

    Remove docs, as it's not necessary

--- docs/next.config.mjs
+++
@@ -1,8 +0,0 @@
-import withNextra from 'nextra'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-export default withNextra({ theme: 'nextra-theme-docs', themeConfig: './theme.config.tsx' })(
-  nextConfig
-)
